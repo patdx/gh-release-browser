caad01178927a2fb9f2576ff07110f2e0318ba57
Patrick Miller <pamiller.pdx@gmail.com>
2025-04-25 07:01:21 +0000

    Migrate to workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import build from '@hono/vite-cloudflare-pages';
-import devServer from '@hono/vite-dev-server';
-import adapter from '@hono/vite-dev-server/cloudflare';
-import { defineConfig } from 'vite';
-import { cjsInterop } from 'vite-plugin-cjs-interop';
+import { defineConfig } from "vite";
+import { cjsInterop } from "vite-plugin-cjs-interop";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    build({}),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx',
-    }),
+    cloudflare(),
     cjsInterop({
-      dependencies: ['semver'],
+      dependencies: ["semver"],
     }),
   ],
 });
